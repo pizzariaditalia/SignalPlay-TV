88e5d5d43f610f7b56a74d700c40d1c90caebca4
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-25 14:58:19 -0300

    Criar app/src/main/java/com/tv/signalplay/MainActivity.kt

---
+++ app/src/main/java/com/tv/signalplay/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tv.signalplay
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // A nossa Ecrã Nativa de TV será construída aqui!
+    }
+}
