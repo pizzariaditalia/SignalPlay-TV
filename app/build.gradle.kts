7aacd59a0119b7468514dce8a39cb9c03ff793b1
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-26 00:43:20 -0300

    Atualizar o build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,13 +23,15 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.leanback:leanback:1.0.0")
+    
+    // 🎬 O Motor do Player de Vídeo Nativo
     implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+    
+    // Imagens
     implementation("com.github.bumptech.glide:glide:4.15.1")
     
-    // O motor do banco de dados do seu Painel ADM
+    // Firebase e API
     implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
-    
-    // O Carteiro para falar com a API do Xtream Codes
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
