c1a706007ffdd2d15222b3d20b22e149faccb3c7
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-25 23:43:53 -0300

    Atualizar o XtreamApi.kt

--- app/src/main/java/com/tv/signalplay/XtreamApi.kt
+++ app/src/main/java/com/tv/signalplay/XtreamApi.kt
@@ -6,15 +6,22 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// Modelo de Filme
 data class XtreamVod(
     val stream_id: Int,
     val name: String,
     val stream_icon: String?,
-    val category_name: String? = null // <- O detalhe que faltava para criar os trilhos premium!
+    val category_name: String? = null,
+    val rating: Double? = 0.0
 )
 
-// Modelo de Canal Ao Vivo
+data class XtreamSerie(
+    val series_id: Int,
+    val name: String,
+    val cover: String?,
+    val category_name: String? = null,
+    val rating: Double? = 0.0
+)
+
 data class XtreamLive(
     val stream_id: Int,
     val name: String,
@@ -25,28 +32,23 @@ data class XtreamLive(
 interface XtreamApiService {
     @GET("player_api.php")
     suspend fun getVodStreams(
-        @Query("username") user: String,
-        @Query("password") pass: String,
-        @Query("action") action: String = "get_vod_streams"
+        @Query("username") user: String, @Query("password") pass: String, @Query("action") action: String = "get_vod_streams"
     ): JsonElement 
 
-    // Chamada para pedir Canais Ao Vivo
+    @GET("player_api.php")
+    suspend fun getSeriesStreams(
+        @Query("username") user: String, @Query("password") pass: String, @Query("action") action: String = "get_series"
+    ): JsonElement 
+
     @GET("player_api.php")
     suspend fun getLiveStreams(
-        @Query("username") user: String,
-        @Query("password") pass: String,
-        @Query("action") action: String = "get_live_streams"
+        @Query("username") user: String, @Query("password") pass: String, @Query("action") action: String = "get_live_streams"
     ): JsonElement
 }
 
 object XtreamClient {
     fun create(baseUrl: String): XtreamApiService {
         val formatUrl = if (baseUrl.endsWith("/")) baseUrl else "$baseUrl/"
-        
-        return Retrofit.Builder()
-            .baseUrl(formatUrl)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(XtreamApiService::class.java)
+        return Retrofit.Builder().baseUrl(formatUrl).addConverterFactory(GsonConverterFactory.create()).build().create(XtreamApiService::class.java)
     }
 }
