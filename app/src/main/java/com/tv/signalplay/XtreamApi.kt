3cd74f05fb6c877c9ed9de46eaad7ff9840c64b0
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-25 21:40:15 -0300

    Atualizar o XtreamApi.kt

--- app/src/main/java/com/tv/signalplay/XtreamApi.kt
+++ app/src/main/java/com/tv/signalplay/XtreamApi.kt
@@ -6,26 +6,39 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// Modelo que representa um filme
+// Modelo de Filme
 data class XtreamVod(
     val stream_id: Int,
     val name: String,
     val stream_icon: String?
 )
 
-// O "Carteiro"
+// NOVO: Modelo de Canal Ao Vivo
+data class XtreamLive(
+    val stream_id: Int,
+    val name: String,
+    val stream_icon: String?
+)
+
 interface XtreamApiService {
     @GET("player_api.php")
     suspend fun getVodStreams(
         @Query("username") user: String,
         @Query("password") pass: String,
         @Query("action") action: String = "get_vod_streams"
-    ): JsonElement // Aceita qualquer formato para não travar
+    ): JsonElement 
+
+    // NOVO: Chamada para pedir Canais Ao Vivo
+    @GET("player_api.php")
+    suspend fun getLiveStreams(
+        @Query("username") user: String,
+        @Query("password") pass: String,
+        @Query("action") action: String = "get_live_streams"
+    ): JsonElement
 }
 
 object XtreamClient {
     fun create(baseUrl: String): XtreamApiService {
-        // Garante que a URL sempre termine com uma barra "/"
         val formatUrl = if (baseUrl.endsWith("/")) baseUrl else "$baseUrl/"
         
         return Retrofit.Builder()
