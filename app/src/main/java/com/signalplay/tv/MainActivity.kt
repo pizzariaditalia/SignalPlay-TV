b5a47a55ff4c949b52ae8c6d5ca3e0fe8bf24ebf
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-29 22:07:32 -0300

    Criar app/src/main/java/com/signalplay/tv/MainActivity.kt

---
+++ app/src/main/java/com/signalplay/tv/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.signalplay.tv
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Por enquanto, o app apenas inicia sem carregar nenhum design visual extra.
+        // Assim garantimos que a estrutura base está rodando 100%!
+    }
+}
