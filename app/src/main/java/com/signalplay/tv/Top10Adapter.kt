cc88bc792a45f3fd50931470ade35fd4ee7b2c74
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-05-31 00:44:25 -0300

    Atualizar o Top10Adapter.kt

--- app/src/main/java/com/signalplay/tv/Top10Adapter.kt
+++ app/src/main/java/com/signalplay/tv/Top10Adapter.kt
@@ -33,12 +33,13 @@ class Top10Adapter(
         val options = RequestOptions().transform(CenterCrop(), RoundedCorners(8))
         Glide.with(holder.itemView.context).load(item.urlImagem).apply(options).into(holder.cardImage)
 
-        // EFEITO NETFLIX
+        // SUPER ZOOM E SOMBRA ATIVADOS
         holder.itemView.setOnFocusChangeListener { view, hasFocus ->
             if (hasFocus) {
-                view.animate().scaleX(1.05f).scaleY(1.05f).translationZ(10f).setDuration(150).start()
+                view.bringToFront()
+                view.animate().scaleX(1.12f).scaleY(1.12f).translationZ(20f).setDuration(200).start()
             } else {
-                view.animate().scaleX(1f).scaleY(1f).translationZ(0f).setDuration(150).start()
+                view.animate().scaleX(1f).scaleY(1f).translationZ(0f).setDuration(200).start()
             }
         }
 
