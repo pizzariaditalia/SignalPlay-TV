e1cd615343cc51d2ad81f312f3632d7101bf9eea
devwebapp <61949944+pizzariaditalia@users.noreply.github.com>
2026-06-04 08:17:41 -0300

    Criar CategoriaEntity.kt

---
+++ app/src/main/java/com/signalplay/tv/CategoriaEntity.kt
@@ -0,0 +1,11 @@
+package com.signalplay.tv
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "categorias")
+data class CategoriaEntity(
+    @PrimaryKey val id: String,
+    val nome: String,
+    val tipo: String // "live", "vod" ou "series"
+)
